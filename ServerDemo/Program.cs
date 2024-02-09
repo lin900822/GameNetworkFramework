7759aebab1f5fb036447f976a6cd1c53b97bcc29
lin900822 <wilson24386433@gmail.com>
2024-02-09 23:38:35 +0800

    feat: HeartBeat改成5分鐘

--- ServerDemo/Program.cs
+++ ServerDemo/Program.cs
@@ -22,7 +22,7 @@ try
         ServerName      = "DemoServer",
         Port            = 10001,
         MaxSessionCount = 2000,
-        HeartBeat       = 120,
+        HeartBeat       = 300,
     });
     serviceCollection.AddSingleton<DemoServer>();
 
